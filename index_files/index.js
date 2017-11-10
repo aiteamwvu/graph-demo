687ec5c8f363f90e57c89a4046c8d9bde9bf0866
Carlos Toxtli <ctoxtli@gmail.com>
2017-11-10 12:57:19 -0500

    Configuration values added

--- index_files/index.js
+++ index_files/index.js
@@ -4,14 +4,28 @@ const Graph = ForceGraph3D()(document.getElementById("3d-graph"));
 loadGraph(url);
 
 function loadGraph(url) {
-        Graph
-		    .cooldownTicks(1000)
-		    .cooldownTime(20000)
-		    .nodeRelSize(5)
-	    	.idField('id')
-            .nameField('id')
-            //.valField('group')
-            .autoColorBy('group')
-            .forceEngine('ngraph')
-            .jsonUrl(url);
+	Graph.resetProps();
+	Graph
+		.warmupTicks(200)
+	    .cooldownTicks(1000)
+	    .cooldownTime(15000)
+	    .nodeRelSize(5)
+	    .numDimensions(3)
+	    .nodeResolution(8)
+	    .lineOpacity(0.2)
+		.idField('id')
+	    .nameField('id')
+	    //.valField('group')
+	    //.colorField('color')
+	    .linkSourceField('source')
+	    .linkTargetField('target')
+	    //.linkColorField('color')
+	    .onNodeClick(onNodeClick)
+	    .autoColorBy('group')
+	    .forceEngine('ngraph')
+	    .jsonUrl(url);
+}
+
+function onNodeClick(node) {
+	console.log(node);
 }
