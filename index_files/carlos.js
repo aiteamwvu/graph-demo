0a0dc5043d6c195ea0f9da08b3c534f091eedb98
Carlos Toxtli <ctoxtli@gmail.com>
2017-11-10 15:19:20 -0500

    Lines highlight

--- index_files/carlos.js
+++ index_files/carlos.js
@@ -1,6 +1,13 @@
 function onNodeClick(node) {
 	console.log("SINGLE CLICK");
 	console.log(node);
+	if (node.type == "line") {
+		if (node.__line.material.opacity == 1) {
+			node.__line.material.opacity = 0.2;
+		} else {
+			node.__line.material.opacity = 1;
+		}
+	}
 }
 
 function onNodeDblClick(node) {
